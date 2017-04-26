e4e2b03058c9cbb716f51bbb815b4969f5a4315c
Ken Tran <tranhongocdung@gmail.com>
2017-04-26 21:34:38 +0700

    Fix print layout

--- HaiTrieuWebApp/MVCWeb/Models/OrderPrintModel.cs
+++ HaiTrieuWebApp/MVCWeb/Models/OrderPrintModel.cs
@@ -10,8 +10,22 @@ namespace MVCWeb.Models
         //Order
         public Order Order { get; set; }
 
-        public int TotalCash => Order.OrderDetails.Sum(o => o.Quantity*o.UnitPrice);
-        public int Discount => (Order.DiscountType == 0 ? (TotalCash * Order.DiscountValue /100) : Order.DiscountValue);
-        public int FinalCash => (TotalCash - Discount);
+        public int TotalCash
+        {
+            get { return Order.OrderDetails.Sum(o => o.Quantity*o.UnitPrice); }
+        }
+
+        public int Discount
+        {
+            get { return (Order.DiscountType == 0 ? (TotalCash*Order.DiscountValue/100) : Order.DiscountValue); }
+        }
+
+        public int FinalCash
+        {
+            get
+            {
+                return (TotalCash - Discount);
+            }
+        }
     }
 }
